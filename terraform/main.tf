91c2ef3a6224e2e563f1e377f0f631b060e143fc
d.pilatsis@ll-games.com <d.pilatsis@ll-games.com>
2024-01-23 21:27:30 +0400

    1 lesson: project structure and vault

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+    ansiblevault = {
+      source  = "MeilleursAgents/ansiblevault"
+      version = "~> 2.0"
+    }
+  }
+}
